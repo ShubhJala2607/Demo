9267d34504a5ea108c83dc1c815b2724d408334f
ShubhJala2607 <shubh.jala@dcmail.ca>
2024-05-23 21:49:40 -0400

    Add files via upload

---
+++ FaultyCalculator.py
@@ -0,0 +1,27 @@
+#excerise2
+val1= int(input("Enter a Number:"))
+val2= int(input("Enter 2nd Number"))
+operator= input("Enter A Operator: +,-,*,/")
+
+if operator== "+":
+    if val1 == 56 and val2 == 9:
+        print("77")
+    else:
+        print("sum is", val1 + val2)
+if operator == "*":
+    if val1 ==45 and val2 == 3:
+        print("555")
+    else:
+        print("multiplication is", val1*val2)
+if operator == "/":
+    if val1 ==56 and val2 == 6:
+        print("4")
+    else:
+        print("division is", float(val1/val2))
+if operator == "-":
+    print("substract is", val1-val2)
+
+
+
+
+
